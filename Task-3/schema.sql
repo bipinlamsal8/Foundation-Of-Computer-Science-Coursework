f0da8ac384db9ad2d07ae480d0741a786bf9a182
bipinlamsal8 <lamsalking9851@gmail.com>
2026-02-24 22:56:23 +0545

    membership values were inserted

--- Task-3/schema.sql
+++ Task-3/schema.sql
@@ -48,3 +48,15 @@ INSERT INTO Club VALUES (1, 'Music Club',  'R101', 'Mr. Raman');
 INSERT INTO Club VALUES (2, 'Sports Club', 'R202', 'Ms. Sita');
 INSERT INTO Club VALUES (3, 'Drama Club',  'R303', 'Mr. Kiran');
 INSERT INTO Club VALUES (4, 'Coding Club', 'Lab1', 'Mr. Anil');
+
+-- Insert Memberships
+INSERT INTO Membership VALUES (1,  1, 1, '2024-01-10');
+INSERT INTO Membership VALUES (2,  2, 2, '2024-01-12');
+INSERT INTO Membership VALUES (3,  1, 2, '2024-01-15');
+INSERT INTO Membership VALUES (4,  3, 1, '2024-01-20');
+INSERT INTO Membership VALUES (5,  4, 3, '2024-01-18');
+INSERT INTO Membership VALUES (6,  5, 1, '2024-01-22');
+INSERT INTO Membership VALUES (7,  2, 3, '2024-01-25');
+INSERT INTO Membership VALUES (8,  6, 2, '2024-01-27');
+INSERT INTO Membership VALUES (9,  3, 4, '2024-01-28');
+INSERT INTO Membership VALUES (10, 7, 4, '2024-01-30');
